015e0be14316f09191ce166b5c80a2b647a63ead
olivier-geiger <69504966+olivier-geiger@users.noreply.github.com>
2021-03-15 19:07:30 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Marked from 'marked';
+import './App.css';
+
+function App() {
+
+  // State
+  const [texte, setTexte] = useState('# `Prévisualisateur`\n### Oh ! De la magie !\nEn réalité, pas vraiment : ceci s\'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !\n\n### Cas d\'utilisation\n* *italique*\n* **gras**\n* `monospace`\n\n* # h1\n* ## h2\n* ### h3\n* #### etc\n\n[Olivier](https://oliviergeiger.com)');
+
+  // Fonctions
+  const changeTextHandler = event => {
+    setTexte(event.target.value);
+  };
+
+  const markdownToHTML = () => {
+    return {__html: Marked(texte)};
+  }
+
+  return (
+    <div className="App">
+      <div className="elements">
+
+        <div className="element">
+          <textarea
+            rows="30"
+            value={texte}
+            onChange={(e) => changeTextHandler(e)}>
+          </textarea>
+        </div>
+
+        <div className="element">
+          <div dangerouslySetInnerHTML={markdownToHTML()}></div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
